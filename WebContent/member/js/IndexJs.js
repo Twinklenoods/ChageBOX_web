7b448ce8a275a92abc1886772b931436f86ff4ce
Twinkle <seed6408@gmail.com>
2020-08-13 12:42:46 +0800

    02

---
+++ WebContent/member/js/IndexJs.js
@@ -0,0 +1,24 @@
+$(function() {
+		    /* 按下GoTop按鈕時的事件 */
+		    $('#gotop').click(function(){
+		        $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
+		        return false;alert('fieldRepopulate');
+		    });
+		    
+		    /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
+		    $(window).scroll(function() {
+		        if ( $(this).scrollTop() > 400){
+		            $('#gotop').fadeIn(333);
+		        } else {
+		            $('#gotop').fadeOut('slow');
+		        }
+		    });
+		});
+		
+$(function() {
+		    /* 按下GoTop按鈕時的事件 */
+		    $('#tab0,#tab-a, #tab-b').click(function(){
+		        $('html,body').animate({ scrollTop: 300 }, 'slow');   /* 返回到最頂上 */
+		        
+		    });
+		});
